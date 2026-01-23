62b3ef84845eb67434215aeeb9a27f283fd328ea
JaaumG <mail@joaoguilherme.dev>
2026-01-23 10:36:51 -0300

    refactor(Pawn): remove atributo `startPosition` e ajusta lógica de verificação para `hasMoved`

--- src/main/java/dev/joao_guilherme/chess/pieces/Pawn.java
+++ src/main/java/dev/joao_guilherme/chess/pieces/Pawn.java
@@ -9,23 +9,17 @@ public final class Pawn extends Piece {
 
     private static final int FIRST_MOVE_DISTANCE = 2;
     private static final int SECOND_MOVE_DISTANCE = 1;
-    private final Position startPosition;
 
     public Pawn(Color color, Position position) {
         super(color, "pawn", position);
-        this.startPosition = position;
     }
 
     @Override
     public boolean isValidMove(Position newPosition) {
         return (isUpward(this.position, newPosition, this.color)
-               && distance(this.position, newPosition) <= (isFirstMove() ? FIRST_MOVE_DISTANCE : SECOND_MOVE_DISTANCE)
+               && distance(this.position, newPosition) <= (hasMoved ? FIRST_MOVE_DISTANCE : SECOND_MOVE_DISTANCE)
                && noPieceAtTarget(newPosition))
                || (isDiagonal(this.position, newPosition) && distance(this.position, newPosition) == 2
                 && noSameColorPieceAtTarget(this.color, newPosition) && !noPieceAtTarget(newPosition));
-    }
-
-    private boolean isFirstMove() {
-        return startPosition.equals(this.position);
     }
 }
