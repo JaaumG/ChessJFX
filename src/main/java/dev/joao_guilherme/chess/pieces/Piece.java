a389b52bc6fe962bdf18e80edf0f679261150c2a
JaaumG <mail@joaoguilherme.dev>
2026-01-23 08:56:20 -0300

    feat(Piece): cria classe base abstrata para peças de xadrez com lógica comum e suporte a movimentos

---
+++ src/main/java/dev/joao_guilherme/chess/pieces/Piece.java
@@ -0,0 +1,62 @@
+package dev.joao_guilherme.chess.pieces;
+
+import dev.joao_guilherme.chess.board.Position;
+import dev.joao_guilherme.chess.enums.Color;
+
+import java.util.Objects;
+
+public abstract sealed class Piece permits Bishop, King, Knight, Queen, Pawn, Rook {
+
+    final Color color;
+    final String name;
+    final String symbolPath;
+    Position position;
+
+    protected Piece(Color color, String name, Position position) {
+        this.color = color;
+        this.name = name;
+        this.position = position;
+        this.symbolPath = "/pieces/%s-%s.png".formatted(name, color.name().toLowerCase().charAt(0));
+    }
+
+    public abstract boolean isValidMove(Position newPosition);
+
+    public void moveTo(Position newPosition) {
+        if (isValidMove(newPosition)) {
+            this.position = newPosition;
+        }
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSymbolPath() {
+        return symbolPath;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public boolean isSameColor(Piece piece) {
+        return this.color == Objects.requireNonNull(piece).getColor();
+    }
+
+    public boolean isNotSameColor(Piece piece) {
+        return !isSameColor(piece);
+    }
+
+    public boolean isNotSameColor(Color color) {
+        return !this.color.equals(color);
+    }
+
+    @Override
+    public String toString() {
+        return "%s-%s".formatted(name, position);
+    }
+}
