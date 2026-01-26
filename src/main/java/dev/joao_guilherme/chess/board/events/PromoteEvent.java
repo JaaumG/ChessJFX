7904cb296777548a1956c96b1a694599ff7cb61c
JaaumG <mail@joaoguilherme.dev>
2026-01-26 10:34:44 -0300

    feat(Events): criado evento para promoção de peão (PromoteEvent)

---
+++ src/main/java/dev/joao_guilherme/chess/board/events/PromoteEvent.java
@@ -0,0 +1,8 @@
+package dev.joao_guilherme.chess.board.events;
+
+import dev.joao_guilherme.chess.board.Position;
+import dev.joao_guilherme.chess.pieces.Pawn;
+import dev.joao_guilherme.chess.pieces.Piece;
+
+public record PromoteEvent(Pawn pawn, Piece promotedPiece, Position previousPosition, Position promotedPosition) {
+}
