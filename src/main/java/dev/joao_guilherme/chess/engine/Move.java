aea32a93c4279ce511b7c36c981b6a862e7184b5
JaaumG <mail@joaoguilherme.dev>
2026-01-26 22:02:09 -0300

    feat(engine): adicionada classe `Move` para representar movimentações de peças

---
+++ src/main/java/dev/joao_guilherme/chess/engine/Move.java
@@ -0,0 +1,7 @@
+package dev.joao_guilherme.chess.engine;
+
+import dev.joao_guilherme.chess.board.Position;
+import dev.joao_guilherme.chess.pieces.Piece;
+
+public record Move(Piece piece, Position to, float eval) {
+}
