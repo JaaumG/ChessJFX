408fb92f64720a6f19a87b5282303052b3d04ef9
JaaumG <mail@joaoguilherme.dev>
2026-01-27 18:45:11 -0300

    feat(PositionEvaluator): adicionada classe para avaliação de posições no tabuleiro com base em tabelas de valores

---
+++ src/main/java/dev/joao_guilherme/chess/engine/PositionEvaluator.java
@@ -0,0 +1,113 @@
+package dev.joao_guilherme.chess.engine;
+
+import dev.joao_guilherme.chess.board.Board;
+import dev.joao_guilherme.chess.enums.Color;
+import dev.joao_guilherme.chess.pieces.*;
+
+import java.util.Objects;
+
+public class PositionEvaluator {
+
+    private static final int[] PAWN_PST = {
+            0,  0,  0,  0,  0,  0,  0,  0,
+            50, 50, 50, 50, 50, 50, 50, 50,
+            10, 10, 20, 30, 30, 20, 10, 10,
+            5,  5, 10, 25, 25, 10,  5,  5,
+            0,  0,  0, 20, 20,  0,  0,  0,
+            5, -5,-10,  0,  0,-10, -5,  5,
+            5, 10, 10,-20,-20, 10, 10,  5,
+            0,  0,  0,  0,  0,  0,  0,  0
+    };
+    private static final int[] KNIGHT_PST = {
+            -50,-40,-30,-30,-30,-30,-40,-50,
+            -40,-20,  0,  0,  0,  0,-20,-40,
+            -30,  0, 10, 15, 15, 10,  0,-30,
+            -30,  5, 15, 20, 20, 15,  5,-30,
+            -30,  0, 15, 20, 20, 15,  0,-30,
+            -30,  5, 10, 15, 15, 10,  5,-30,
+            -40,-20,  0,  5,  5,  0,-20,-40,
+            -50,-40,-30,-30,-30,-30,-40,-50
+    };
+    private static final int[] BISHOP_PST = {
+            -20,-10,-10,-10,-10,-10,-10,-20,
+            -10,  0,  0,  0,  0,  0,  0,-10,
+            -10,  0,  5, 10, 10,  5,  0,-10,
+            -10,  5,  5, 10, 10,  5,  5,-10,
+            -10,  0, 10, 10, 10, 10,  0,-10,
+            -10, 10, 10, 10, 10, 10, 10,-10,
+            -10,  5,  0,  0,  0,  0,  5,-10,
+            -20,-10,-10,-10,-10,-10,-10,-20
+    };
+    private static final int[] ROOK_PST = {
+            0,  0,  0,  0,  0,  0,  0,  0,
+            5, 10, 10, 10, 10, 10, 10,  5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            -5,  0,  0,  0,  0,  0,  0, -5,
+            0,  0,  0,  5,  5,  0,  0,  0
+    };
+    private static final int[] QUEEN_PST = {
+            -20,-10,-10, -5, -5,-10,-10,-20,
+            -10,  0,  0,  0,  0,  0,  0,-10,
+            -10,  0,  5,  5,  5,  5,  0,-10,
+            -5,  0,  5,  5,  5,  5,  0, -5,
+            0,  0,  5,  5,  5,  5,  0, -5,
+            -10,  5,  5,  5,  5,  5,  0,-10,
+            -10,  0,  5,  0,  0,  0,  0,-10,
+            -20,-10,-10, -5, -5,-10,-10,-20
+    };
+    private static final int[] KING_MID_PST = {
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -30,-40,-40,-50,-50,-40,-40,-30,
+            -20,-30,-30,-40,-40,-30,-30,-20,
+            -10,-20,-20,-20,-20,-20,-20,-10,
+            20, 20,  0,  0,  0,  0, 20, 20,
+            20, 30, 10,  0,  0, 10, 30, 20
+    };
+    private static final int[] KING_END_PST = {
+            -50,-40,-30,-20,-20,-30,-40,-50,
+            -30,-20,-10,  0,  0,-10,-20,-30,
+            -30,-10, 20, 30, 30, 20,-10,-30,
+            -30,-10, 30, 40, 40, 30,-10,-30,
+            -30,-10, 30, 40, 40, 30,-10,-30,
+            -30,-10, 20, 30, 30, 20,-10,-30,
+            -30,-30,  0,  0,  0,  0,-30,-30,
+            -50,-30,-30,-30,-30,-30,-30,-50
+    };
+
+    public static int evaluatePiecePosition(Board board, Piece piece) {
+        boolean whiteHasQueen = hasPiece(board, Color.WHITE, Queen.class);
+        boolean blackHasQueen = hasPiece(board, Color.BLACK, Queen.class);
+        boolean isEndGame = !whiteHasQueen && !blackHasQueen;
+
+        boolean isWhite = piece.getColor() == Color.WHITE;
+
+        int index = (piece.getPosition().getRow() - 1) * 8 + piece.getPosition().getColumn();
+
+        int tableIndex = isWhite ? flip(index) : index;
+
+        return (isWhite ? 1 : -1) * switch (piece) {
+            case Pawn _ -> PAWN_PST[tableIndex];
+            case Knight _ -> KNIGHT_PST[tableIndex];
+            case Bishop _ -> BISHOP_PST[tableIndex];
+            case Rook _ -> ROOK_PST[tableIndex];
+            case Queen _ -> QUEEN_PST[tableIndex];
+            case King _ -> isEndGame ? KING_END_PST[tableIndex] : KING_MID_PST[tableIndex];
+        };
+    }
+
+    private static int flip(int index) {
+        return index ^ 56;
+    }
+
+    private static boolean hasPiece(Board board, Color color, Class<? extends Piece> type) {
+        for (Piece p : Objects.requireNonNull(board).getPieces(color)) {
+            if (type.isInstance(p)) return true;
+        }
+        return false;
+    }
+}
