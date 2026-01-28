c165a41963664232f36a0215885898aede623120
JaaumG <mail@joaoguilherme.dev>
2026-01-28 09:30:49 -0300

    feat(ChessApp): reorganiza a estrutura da interface com BorderPane e adiciona painéis laterais para avaliação e controle de turno

--- src/main/java/dev/joao_guilherme/chess/ui/ChessApp.java
+++ src/main/java/dev/joao_guilherme/chess/ui/ChessApp.java
@@ -2,18 +2,28 @@ package dev.joao_guilherme.chess.ui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class ChessApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        BoardView board = BoardView.getInstance();
-        Scene scene = new Scene(board, board.getWidth(), board.getHeight());
+        BorderPane root = new BorderPane();
 
+        BoardView boardView = BoardView.getInstance();
+        StackPane boardContainer = new StackPane(boardView);
+        boardContainer.setStyle("-fx-background-color: #222;");
+        SidePanel rightPanel = new SidePanel();
+        EvaluationBar leftPanel = new EvaluationBar();
+        root.setCenter(boardContainer);
+        root.setRight(rightPanel);
+        root.setLeft(leftPanel);
+        Scene scene = new Scene(root, boardView.getWidth() + rightPanel.getWidth() + leftPanel.getWidth(), boardView.getHeight());
         primaryStage.setTitle("Chess Game - JavaFX");
         primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
+        primaryStage.centerOnScreen();
         primaryStage.show();
     }
 }
