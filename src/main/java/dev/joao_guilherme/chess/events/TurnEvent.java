fee53ea2b88527511eb4c4729823e9ccdd7bf46f
JaaumG <mail@joaoguilherme.dev>
2026-01-30 14:19:19 -0300

    feat(TurnEvent): adiciona atributo lastMove para registrar o último movimento realizado

--- src/main/java/dev/joao_guilherme/chess/events/TurnEvent.java
+++ src/main/java/dev/joao_guilherme/chess/events/TurnEvent.java
@@ -1,6 +1,7 @@
 package dev.joao_guilherme.chess.events;
 
 import dev.joao_guilherme.chess.enums.Color;
+import dev.joao_guilherme.chess.movements.MoveRecord;
 
-public record TurnEvent(Color color) implements GameEvent {
+public record TurnEvent(Color color, MoveRecord lastMove) implements GameEvent {
 }
